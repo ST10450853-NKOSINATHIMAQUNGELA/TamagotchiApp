a3648ac4dd34d92d0e8c9d20b64bc495a9b47418
ST10450853-NKOSINATHIMAQUNGELA <ST10450853@vcconnect.edu.za>
2024-05-01 00:17:42 +0200

    Added variables to the second page code

---
+++ app/src/main/java/com/example/tamagotchiapp/StartPage.kt
@@ -0,0 +1,35 @@
+package com.example.tamagotchiapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class StartPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_start_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val imageView = findViewById<ImageView>(R.id.avatar)
+
+        val playButton = findViewById<Button>(R.id.playButton)
+
+        val feedButton = findViewById<Button>(R.id.feedButton)
+
+        val bathButton = findViewById<Button>(R.id.BathButton)
+
+
+
+
+
+    }
+}
